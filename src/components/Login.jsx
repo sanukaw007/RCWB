625c534365b249920c3112c32fee01e32ad5a77e
sanukaw007 <sanuka.w007+gh@gmail.com>
2025-05-05 12:29:46 +0530

    Test fix login scrolling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,6 +37,18 @@ export default function Login({ setisAdmin }) {
     });
 
   }, [])
+  
+  useEffect(() => {
+    const setVh = () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    setVh();
+    window.addEventListener('resize', setVh);
+
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+
 
   const onLogin = (e) => {
     e.preventDefault();
